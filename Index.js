239013855701a0ca57d72b284d1e15a02e94d588
pseudonym-26693 <55923733+pseudonym-2669@users.noreply.github.com>
2020-08-14 13:15:28 +0200

    Fix operations

--- Index.js
+++ Index.js
@@ -4,15 +4,11 @@ const Client = new Discord.Client;
 const prefix = "$"; 
 
 Client.on("ready", () => {
-    console.log("Bot opérationel")
-});
-
-Client.on('ready', () => {
+    console.info("Bot opérationel")
     console.info(`[MAIN] Logged in as ${Client.user.tag}! [MAIN]`); // info log on utilise console.info
-    Client.user.setActivity("$help")
-    console.info("L'activité joue a $help a bien était mise")
 });
 
 Client.login("Le_token")
+
 
 //les commandes
