2c7d2e810f8a0b43a88040d624abe9932ed5940d
pseudonym-26693 <55923733+pseudonym-2669@users.noreply.github.com>
2020-08-14 14:40:51 +0200

    Add kick and ban commands

--- Index.js
+++ Index.js
@@ -12,3 +12,31 @@ Client.login("Le_token")
 
 
 //les commandes
+
+client.on("message", message => {
+if (message.content.startsWith(prefix + "kick")) {
+    if(!message.member.roles.some(r=>["Admin"].includes(r.name)) ) // on verifie les roles
+      return null
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Le membre n'existe pas");
+    if(!member.bannable) 
+      message.reply("impossible de kicker cet utilisateur veuillez verifier les permissions")
+    
+    member.kick()
+    message.reply(`${member.user.tag} a etait kicker par ${message.author.tag} pour raison: ${reason}`);
+}})
+
+client.on("message", message => {
+if (message.content.startsWith(prefix + "ban")) {
+    if(!message.member.roles.some(r=>["Admin"].includes(r.name)) ) // on verifie les roles
+      return null
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Le membre n'existe pas");
+    if(!member.bannable) 
+      message.reply("impossible de bannir cet utilisateur veuillez verifier les permissions")
+    
+    member.kick()
+    message.reply(`${member.user.tag} a etait banni par ${message.author.tag} pour raison: ${reason}`);
+}})
