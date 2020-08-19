c2260df9e95e392622a4d3096f0324665e265459
pseudonym-26693 <55923733+pseudonym-2669@users.noreply.github.com>
2020-08-19 16:13:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 npm install discord.js # install discord.js
 npm install canvas # install canvas
+npm init -y # init de package-lock.json
 npm install nodemon --save-dev # install nodemon to run
 
 npm run dev # run
