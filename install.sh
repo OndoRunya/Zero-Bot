124b9e76b73a9d5bedd90e50c1355f2ae298f8b4
pseudonym-26693 <55923733+pseudonym-2669@users.noreply.github.com>
2020-08-26 10:09:33 +0200

    Fix installation

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 npm install discord.js # install discord.js
-npm install canvas # install canvas
 npm init -y # init de package-lock.json
 npm install nodemon --save-dev # install nodemon to run
 
