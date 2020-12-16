4abfca7da5f10fa432e318db737ee2f0a9bf6842
pseudonym-2669 <55923733+pseudonym-2669@users.noreply.github.com>
2020-12-16 20:52:04 +0100

    Make clean

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo aptitude install npm # install npm
 sudo npm install discord.js # install discord.js
 sudo npm init -y # init de package-lock.json
 sudo npm install nodemon --save-dev # install nodemon to run
@@ -15,5 +16,5 @@ elif [[ a == "n" ]]
 then
     echo "Annule (pour run : npm run dev)"
 else
-    echo "Je ne comprend pas"
+    echo "Error"
 fi
