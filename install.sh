bcc654df780ec216005f8119a3298cb5df8ab22e
pseudonym-2669 <55923733+pseudonym-2669@users.noreply.github.com>
2021-06-22 15:59:39 +0200

    Correct "sudo"

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo aptitude install npm # install npm
-sudo npm install discord.js # install discord.js
-sudo npm init -y # init de package-lock.json
-sudo npm install nodemon --save-dev # install nodemon to run
+npm install discord.js # install discord.js
+npm init -y # init de package-lock.json
+npm install nodemon --save-dev # install nodemon to run
 sudo npm install -g npm # update (using sudo)
 
 echo "Avez vous configurer votre token dans config.json ? Si oui (y) si non (n)"
