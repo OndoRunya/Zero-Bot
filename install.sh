022a917f6c1479fcb8fa9cac0325a79872fd731c
pseudonym-2669 <55923733+pseudonym-2669@users.noreply.github.com>
2021-07-31 17:55:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+sleep 2
+echo "${green}[INSTALLER] Installing Zero-Bot${reset}"
+
+
 sudo aptitude install npm # install npm
 npm install discord.js # install discord.js
 npm init -y # init de package-lock.json
 npm install nodemon --save-dev # install nodemon to run
 sudo npm install -g npm # update (using sudo)
 
-echo "Avez vous configurer votre token dans config.json ? Si oui (y) si non (n)"
-read a
-
-if [[ a == "y" ]]
-then
-    npm run dev # run
-elif [[ a == "n" ]]
-then
-    echo "Annule (pour run : npm run dev)"
-else
-    echo "Error"
-fi
+sleep 2
+echo "${green}[INSTALLER] Succesfully installed Zero-Bot${reset}"
